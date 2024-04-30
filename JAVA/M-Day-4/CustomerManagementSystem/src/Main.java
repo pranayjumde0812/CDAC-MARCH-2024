18de14f2cdd476e12a0de3465fb695fa56f75125
Pranay Jumde <pranayjumde13@gmail.com>
2024-05-01 02:21:32 +0530

    COmpleted normal functionalities of CMS application (Without validations)

---
+++ JAVA/M-Day-4/CustomerManagementSystem/src/Main.java
@@ -0,0 +1,59 @@
+import com.app.cms.Customer;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static com.app.utility.CustomerUtility.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<Customer> customers = new ArrayList<>();
+        try (Scanner sc = new Scanner(System.in)) {
+            int choice;
+            do {
+                System.out.println("Enter the choice");
+                System.out.println("1.Sign-Up | 2.Sign-In(Login) | 3. Change Password");
+                System.out.println("4.Un-subscribe | 5.Display all customers | 0. Exit");
+                choice = sc.nextInt();
+                try {
+                    switch (choice) {
+                        case 1:
+                            customers.add(acceptCustomerDetails());
+                            System.out.println("Customer register successfully");
+                            break;
+                        case 2:
+                            System.out.println("Enter your Email");
+                            Customer cust = checkRegisteredCustomer(customers, sc.next());
+                            System.out.println("Enter your Password");
+                            String msg = loginTOAccount(cust, sc.next());
+                            System.out.println(msg);
+
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            System.out.println("Enter customer email to unsubscribe");
+                            Customer c = checkRegisteredCustomer(customers, sc.next());
+                            String str = customerUnsubscribe(c);
+                            System.out.println(str);
+                            break;
+                        case 5:
+                            for (Customer customer : customers) {
+                                if (customer != null) {
+                                    System.out.println(customer.getFirstName() + " " + customer.getLastName() + " " + customer.getDateOfBirth());
+                                }
+                            }
+                            break;
+                        case 0:
+                            System.out.println("Exiting.....");
+                            break;
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    sc.nextLine();
+                }
+            } while (choice != 0);
+        } // end of try with resource
+    }
+}
---
+++ JAVA/M-Day-4/CustomerManagementSystem/src/com/app/utility/CustomerUtility.java
@@ -0,0 +1,56 @@
+package com.app.utility;
+
+import com.app.cms.Customer;
+import com.app.enums.ServicePlan;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CustomerUtility {
+
+    public static Customer acceptCustomerDetails() throws IllegalArgumentException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter First name : ");
+        String firstName = scanner.next();
+        System.out.println("Enter Last name : ");
+        String lastName = scanner.next();
+        System.out.println("Enter email : ");
+        String email = scanner.next();
+        System.out.println("Enter Password : ");
+        String pass = scanner.next();
+        System.out.println("Enter Date of Birth :- yyyy-MM-dd ");
+        String dob = scanner.next();
+        System.out.println("Enter service plan");
+        String servicePlan = scanner.next();
+        System.out.println("Enter Registration amount : ");
+        double registrationAmount = scanner.nextDouble();
+
+        return new Customer(firstName, lastName, email, pass, registrationAmount, LocalDate.parse(dob), ServicePlan.valueOf(servicePlan.toUpperCase()));
+    }
+
+    public static Customer checkRegisteredCustomer(ArrayList<Customer> list, String email) throws Exception {
+
+        for (Customer customer : list) {
+            if (customer != null && email.equals(customer.getEmail())) {
+                return customer;
+            }
+        }
+        throw new Exception("Email not registered");
+    }
+
+    public static String loginTOAccount(Customer customer, String password) throws Exception {
+        String string = "";
+        if (customer.getPassword().equals(password)) {
+            string += "Login Successfully";
+        } else {
+            throw new Exception("Invalid Credentials");
+        }
+        return string;
+    }
+
+    public static String customerUnsubscribe(Customer customer) {
+        customer.setSubscribed(false);
+        return "Customer Unsubscribed";
+    }
+}
