4da40be5ed84cfcceb68c815badf1dd04cc82e54
Pranay Jumde <pranayjumde13@gmail.com>
2024-05-10 23:32:10 +0530

    Inter Thread Communication(wait, notify,notifyAll)

---
+++ JAVA/M-Day-13/InterThreadCommunication/src/itc_with_wait_notify/EmpUtils.java
@@ -0,0 +1,31 @@
+package itc_with_wait_notify;
+
+public class EmpUtils {
+	private Emp e;
+	private boolean dataReady;
+
+	public synchronized void writeData(Emp ref) throws Exception {
+		System.out.println("w entered --- " + Thread.currentThread().getName());
+		while (dataReady)
+			this.wait(); //p : blocked on wait -- outside
+		// produce data
+		e = ref;
+		System.out.println("Write Data " + e);
+		System.out.println("w exited --- " + Thread.currentThread().getName());
+		dataReady=true;
+		this.notify();//producer --> wake up signal --> consumer
+	}
+
+	public synchronized Emp readData() throws Exception {
+		System.out.println("r entered --- " + Thread.currentThread().getName());
+		while(!dataReady)
+			this.wait();
+		// consume data
+		System.out.println("Read  Data " + e);
+		System.out.println("r exited --- " + Thread.currentThread().getName());
+		dataReady=false;
+		this.notify();//consumer --> wake up signal --> producer
+		return e;
+	}
+
+}
