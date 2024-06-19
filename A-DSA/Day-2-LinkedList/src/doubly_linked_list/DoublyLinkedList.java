70246591453f184c7c6a185e50d9593cfc7abe1e
Pranay Jumde <pranayjumde13@gmail.com>
2024-06-19 23:04:36 +0530

    Doubly Linked List

---
+++ A-DSA/Day-2-LinkedList/src/doubly_linked_list/DoublyLinkedList.java
@@ -0,0 +1,134 @@
+package doubly_linked_list;
+
+public class DoublyLinkedList {
+
+    private Node head;
+
+    private class Node {
+        int value;
+        Node next;
+        Node prev;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public Node(int value, Node next, Node prev) {
+            this.value = value;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    // Insert First
+    public void insertFirst(int val) {
+        Node node = new Node(val);
+
+        node.next = head;
+        node.prev = null;
+
+        if (head != null) {
+            head.prev = node;
+        }
+
+        head = node;
+    }
+
+    // Insert Last
+    public void insertLast(int val) {
+        Node node = new Node(val);
+        Node last = head;
+        node.next = null;
+
+        if (head == null) {
+            node.prev = null;
+            head = node;
+            return;
+        }
+
+        while (last.next != null) {
+            last = last.next;
+        }
+
+        last.next = node;
+        node.prev = last;
+    }
+
+    // Insert At index
+
+    public void insert(int value, int index) {
+
+        Node node = new Node(value);
+
+        if (index == 0) {
+            insertFirst(value);
+            return;
+        }
+
+        Node last = head;
+
+        for (int i = 1; i < index; i++) {
+            last = last.next;
+        }
+
+        node.next = last.next;
+        node.prev = last;
+
+        if (node.next != null) {
+            node.next.prev = node;
+        }
+        last.next = node;
+    }
+
+    // delete first
+    public int deleteFirst() {
+
+        int val = head.value;
+        head = head.next;
+        if (head != null) {
+            head.prev = null;
+        }
+
+        return val;
+    }
+
+    // delete last
+    public int deleteLast() {
+
+        Node temp = head;
+
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        int val = temp.value;
+
+        temp.prev.next = null;
+
+        return val;
+    }
+
+    public int delete(int index) {
+        Node temp = head;
+
+        for (int i = 1; i <= index; i++) {
+            temp = temp.next;
+        }
+
+        temp.prev.next = temp.next;
+        if (temp.next != null) {
+            temp.next.
+        }
+
+        return temp.value;
+    }
+
+    public void display() {
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.value + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("END");
+    }
+}
---
+++ A-DSA/Day-2-LinkedList/src/doubly_linked_list/Main.java
@@ -0,0 +1,65 @@
+package doubly_linked_list;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DoublyLinkedList dll = new DoublyLinkedList();
+
+        System.out.println("-------------------------------- Insert First ----------------------------------------");
+        dll.insertFirst(10);
+        dll.display();
+        dll.insertFirst(11);
+        dll.display();
+        dll.insertFirst(12);
+        dll.display();
+        dll.insertFirst(13);
+        dll.display();
+        dll.insertFirst(14);
+        dll.display();
+
+        System.out.println("------------------------------- Insert Last -----------------------------------------");
+
+        dll.insertLast(55);
+        dll.display();
+        dll.insertLast(56);
+        dll.display();
+        dll.insertLast(57);
+        dll.display();
+        dll.insertLast(58);
+        dll.display();
+        dll.insertLast(59);
+        dll.display();
+
+        System.out.println("------------------------------- Insert At Specific position -----------------------------------------");
+        dll.insert(222, 8);
+        dll.display();
+        dll.insert(66, 0);
+        dll.display();
+        dll.insert(88, 5);
+        dll.display();
+        dll.insert(121, 7);
+        dll.display();
+        dll.insert(14, 14);
+        dll.display();
+
+        System.out.println("------------------------------- Delete First -----------------------------------------");
+        dll.display();
+        System.out.println("Element removed = " + dll.deleteFirst());
+        dll.display();
+        System.out.println("Element removed = " + dll.deleteFirst());
+        dll.display();
+        System.out.println("Element removed = " + dll.deleteFirst());
+        dll.display();
+        System.out.println("Element removed = " + dll.deleteFirst());
+        dll.display();
+        System.out.println("------------------------------- Delete Last -----------------------------------------");
+        dll.display();
+        System.out.println(dll.deleteLast());
+        dll.display();
+
+        System.out.println("------------------------------- Delete By index -----------------------------------------");
+        dll.display();
+        System.out.println(dll.delete(5));
+
+    }
+}
