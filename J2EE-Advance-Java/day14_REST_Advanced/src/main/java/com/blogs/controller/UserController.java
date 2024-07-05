6ad6f9877dde282c8e922fcae48e1f0dfabad75c
Pranay Jumde <pranayjumde13@gmail.com>
2024-07-05 21:34:50 +0530

    Changes in REST app

--- J2EE-Advance-Java/day14_REST_Advanced/src/main/java/com/blogs/controller/UserController.java
+++ J2EE-Advance-Java/day14_REST_Advanced/src/main/java/com/blogs/controller/UserController.java
@@ -1,9 +1,6 @@
 package com.blogs.controller;
 
-import java.net.http.HttpResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
